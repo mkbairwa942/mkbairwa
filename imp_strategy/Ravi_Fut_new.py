ae1a50a4d0f393c2adea378df75bf2b5cbf12403
mkbairwa <105072780+mkbairwa@users.noreply.github.com>
2024-07-19 20:13:56 +0530

    19-07-2024

--- imp_strategy/Ravi_Fut_new.py
+++ imp_strategy/Ravi_Fut_new.py
@@ -130,7 +130,7 @@ trading_dayss = trading_days_reverse[::-1]
 trading_days = trading_dayss[1:]
 current_trading_day = trading_dayss[0]
 last_trading_day = trading_days[0]
-second_last_trading_day = trading_days[2]
+second_last_trading_day = trading_days[8]
 fifth_last_trading_day = trading_days[5]
 time_change = timedelta(minutes=870) 
 upto_df = timedelta(minutes=930) 
